6e3e0e3be59a95618f6c8c66a919921f91888f33
Andriy Pyvovarchuk <andriy.pyvovarchuk@techmagic.co>
2017-12-27 12:39:23 +0200

    linting code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,32 @@
 $(document).ready(() => {
-  $("a").click(clickA);
-  $(".header__btn").click(clickHeaderBtn);
+  $('a').click(clickA);
+  $('.header__btn').click(clickHeaderBtn);
 });
 
-$(window).on("load resize scroll", function (e) {
+$(window).on('load resize scroll', function() {
   headerSticky();
 });
 
 
 function clickHeaderBtn() {
-  $(this).toggleClass("header__btn_active");
-  $(this).closest('body').toggleClass("header__btn_active");
+  $(this).toggleClass('header__btn_active');
+  $(this).closest('body').toggleClass('header__btn_active');
 }
 
 function clickA(event) {
-  const link = event.currentTarget.getAttribute("href");
-  if (link.length > 1 && link.indexOf("#") === 0) {
+  const link = event.currentTarget.getAttribute('href');
+  if(link.length > 1 && link.indexOf('#') === 0) {
     event.preventDefault();
 
     navbar($(this));
 
     const section = document.querySelector(link);
-    if (section) {
+    if(section) {
       let top = 70;
-      if ($(section).closest('body').width() > 767) {
+      if($(section).closest('body').width() > 767) {
         top = 90;
       }
-      $("html, body").animate({
+      $('html, body').animate({
         scrollTop: $(link).offset().top - top
       }, 500);
       window.location.hash = link;
@@ -36,19 +36,19 @@ function clickA(event) {
 }
 
 function navbar(active) {
-  if (active.hasClass('header__a')) {
+  if(active.hasClass('header__a')) {
     active.closest('li').addClass('header__li_active').siblings('li').removeClass('header__li_active');
     active.closest('body').removeClass('header__btn_active');
-    active.closest('body').find('.header__btn').toggleClass("header__btn_active");
+    active.closest('body').find('.header__btn').toggleClass('header__btn_active');
   }
 }
 
 function headerSticky() {
-  var sticky = $(".header"),
-    scroll = $(window).scrollTop();
-  if (scroll >= 1) {
-    sticky.addClass("header_fixed");
+  const sticky = $('.header');
+  const scroll = $(window).scrollTop();
+  if(scroll >= 1) {
+    sticky.addClass('header_fixed');
   } else {
-    sticky.removeClass("header_fixed");
+    sticky.removeClass('header_fixed');
   }
 }
